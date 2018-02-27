263506ae682dca6e1239db8cfccbcd839cf3fb4e
Qiyang Gu <puddingtea07@hotmail.com>
2018-02-27 16:15:16 -0500

    add writing json config

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -12,8 +13,8 @@ class CyWECLI():
         self.get_input('cywe_path', 'Your local CyWE path: (Don\'t use "~")\n')
         self.get_input('pypy_path', 'Your local pypy path: (Don\'t use "~")\n')
         self.get_input('cc', 'Your C&C folder name: (leave blank for not creating C&C)\n')
+        self.write_config()
         self.processing()
-
 
     def processing(self):
         os.mkdir(self._arg_dict['name'])
@@ -26,7 +27,22 @@ class CyWECLI():
         os.system('cp {}/pypy/sandbox/pypy3-c-sandbox {}/'.format(self._arg_dict['pypy_path'], self._arg_dict['name']))
 
         os.system('mv {}/simple_player_object_types.ini {}/object_types.ini'.format(self._arg_dict['name'], self._arg_dict['name']))
+
+        os.system('cp cw.py {}/'.format(self._arg_dict['name']))
+        os.system('cp cwconfig.json {}/'.format(self._arg_dict['name']))
+
+        print('Finished!')
     
+    def write_config(self):
+        config = None
+        with open('cwconfig.json', 'r') as f:
+            config = json.load(f)
+
+        config['cywe_path'] = os.path.abspath(self._arg_dict['cywe_path'])
+        config['pypy_path'] = os.path.abspath(self._arg_dict['pypy_path'])
+        with open('cwconfig.json', 'w') as f:
+            json.dump(config, f, indent='\t')
+
     def get_input(self, key, prompt):
         tmp = ''
         while True:

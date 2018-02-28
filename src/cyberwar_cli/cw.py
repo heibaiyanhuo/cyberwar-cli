4c9b9aac5d61df30af43ff3e0dc4da1d7e903544
Qiyang Gu <puddingtea07@hotmail.com>
2018-02-27 19:17:00 -0500

    solve the arg confl

--- src/cyberwar_cli/cw.py
+++ src/cyberwar_cli/cw.py
@@ -9,15 +9,15 @@ def processing():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-m', '--mode', choices=['init', 'launch'], required=True)
-    parser.add_argument('-h', '--switch-host', default=CONFIG['network']['switch']['host'])
-    parser.add_argument('-p', '--switch-port', default=CONFIG['network']['switch']['port'])
+    parser.add_argument('--host', default=CONFIG['network']['switch']['host'])
+    parser.add_argument('-p', '--port', default=CONFIG['network']['switch']['port'])
     parser.add_argument('-a', '--playground-address', default=CONFIG['network']['vnic']['playground_address'])
     args = parser.parse_args()
     run(args)
 
 def run(args):
     if args.mode == 'init':
-        command = 'PYTHONPATH={}/python/game/src/ python -m cyberwar.game --init={},{},{}'.format(CONFIG['cywe_path'], args.switch_host, args.switch_port, args.playground_address)
+        command = 'PYTHONPATH={}/python/game/src/ python -m cyberwar.game --init={},{},{}'.format(CONFIG['cywe_path'], args.host, args.port, args.playground_address)
         os.system(command)
     elif args.mode == 'launch':
         command = 'PYTHONPATH={}/python/game/src/ python -m cyberwar.game --pypy={}'.format(CONFIG['cywe_path'], CONFIG['pypy_path'])
